44d7472899ad7def3bd843ef3486d42a2a15966a
AndreiHromau <Andreihromau@gmail.com>
2026-04-19 21:22:20 +0300

    Refactor: UserEventListenerIntegrationTest

--- src/test/java/ru/aston/notification/kafka/UserEventListenerIntegrationTest.java
+++ src/test/java/ru/aston/notification/kafka/UserEventListenerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
+import ru.aston.notification.kafka.dto.UserEvent;
 
 import java.util.Map;
 
