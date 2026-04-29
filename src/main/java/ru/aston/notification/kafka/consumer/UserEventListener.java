70ecafe1b271e5030b96606644fa06406ceb8288
AndreiHromau <Andreihromau@gmail.com>
2026-04-29 13:59:31 +0300

    refactor: UserEventListener, Consumer вызывает сервис, который отправляет уведомление через MailSender.

--- src/main/java/ru/aston/notification/kafka/consumer/UserEventListener.java
+++ src/main/java/ru/aston/notification/kafka/consumer/UserEventListener.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.aston.notification.service.NotificationService;
 import ru.aston.notification.kafka.dto.UserEvent;
 
 @Service
@@ -11,11 +12,18 @@ public class UserEventListener {
 
     private static final Logger log = LoggerFactory.getLogger(UserEventListener.class);
 
+    private final NotificationService notificationService;
+
+    public UserEventListener(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @KafkaListener(
             topics = "${app.kafka.user-events-topic}",
             containerFactory = "userEventKafkaListenerContainerFactory"
     )
     public void onMessage(UserEvent event) {
         log.info("Consumed user event: operation={} email={}", event.operation(), event.email());
+        notificationService.sendUserNotification(event.operation(), event.email());
     }
 }
